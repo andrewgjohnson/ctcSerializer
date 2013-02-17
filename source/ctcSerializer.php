ae08fa56c414ca7f8e1238ae3acc038708619b59
Andrew G. Johnson <andrew@andrewgjohnson.com>
2013-02-16 23:17:30 -0600

    1.0.0 Final

--- source/ctcSerializer.php
+++ source/ctcSerializer.php
@@ -3,13 +3,13 @@
 /*
  * ctcSerializer v1.0.0
  *
- * Copyright (c) 2013 Andrew G. Johnson andrew@andrewgjohnson.com
+ * Copyright (c) 2013 Andrew G. Johnson  <andrew@andrewgjohnson.com>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @author Andrew G. Johnson <andrew@andrewgjohnson.com>
- * @copyright Copyright (c) 2013 Andrew G. Johnson
+ * @copyright Copyright (c) 2013 Andrew G. Johnson <andrew@andrewgjohnson.com>
  * @link http://github.com/ctcSerializer/ctcSerializer
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  * @version 1.0.0
@@ -21,7 +21,7 @@ if (!class_exists('ctcSerializer'))
 {
 	class ctcSerializer
 	{
-		static $default_settings = array(
+		private static $default_settings = array(
 			'addDecl' => false,
 			'defaultTagName' => 'container',
 			'encoding' => 'utf-8',
@@ -30,22 +30,22 @@ if (!class_exists('ctcSerializer'))
 
 		public static function serialize($data = '',$settings = array())
 		{
-			$settings = is_array($settings) ? array_merge(ctcSerializer::$default_settings,$settings) : ctcSerializer::$default_settings;
-
-			$settings['rootName'] = ltrim((string)$settings['rootName'],'1234567890');
-			if (strlen($settings['rootName']) == 0)
-				$settings['rootName'] = ctcSerializer::$default_settings['rootName'];
-
-			$settings['defaultTagName'] = ltrim((string)$settings['defaultTagName'],'1234567890');
-			if (strlen($settings['defaultTagName']) == 0)
-				$settings['defaultTagName'] = ctcSerializer::$default_settings['defaultTagName'];
+			if (is_array($settings))
+			{
+				$settings = array_merge(self::$default_settings,$settings);
+				$settings['defaultTagName'] = self::get_element_name($settings['defaultTagName'],self::$default_settings['defaultTagName']);
+				$settings['encoding'] = trim((string)$settings['encoding']);
+				$settings['rootName'] = self::get_element_name($settings['rootName'],self::$default_settings['rootName']);
+			}
+			else
+				$settings = self::$default_settings;
 
 			$serialized  = '';
 			if ($settings['addDecl'] === true)
-				$serialized .= '<?xml version="1.0" ' . (strlen($settings['encoding']) > 0 ? 'encoding="' . htmlspecialchars((string)$settings['encoding'],ENT_QUOTES) . '" ' : '') . '?>';
-			$serialized .= '<' . ctcSerializer::get_element_name($settings['rootName'],$settings,true) . '>';
-			$serialized .= ctcSerializer::serialize_data($data,$settings);
-			$serialized .= '</' . ctcSerializer::get_element_name($settings['rootName'],$settings,true) . '>';
+				$serialized .= '<?xml version="1.0" ' . (strlen($settings['encoding']) > 0 ? 'encoding="' . htmlspecialchars($settings['encoding'],ENT_QUOTES) . '" ' : '') . '?>';
+			$serialized .= '<' . $settings['rootName'] . '>';
+			$serialized .= self::serialize_data($data,$settings);
+			$serialized .= '</' . $settings['rootName'] . '>';
 			return $serialized;
 		}
 
@@ -59,9 +59,9 @@ if (!class_exists('ctcSerializer'))
 					$serialized = '';
 					foreach ($data as $key => $value)
 					{
-						$serialized .= '<' . ctcSerializer::get_element_name($key,$settings) . '>';
-						$serialized .= ctcSerializer::serialize_data($value,$settings);
-						$serialized .= '</' . ctcSerializer::get_element_name($key,$settings) . '>';
+						$serialized .= '<' . self::get_element_name($key,$settings['defaultTagName']) . '>';
+						$serialized .= self::serialize_data($value,$settings);
+						$serialized .= '</' . self::get_element_name($key,$settings['defaultTagName']) . '>';
 					}
 					return $serialized;
 				case 'boolean':
@@ -69,30 +69,33 @@ if (!class_exists('ctcSerializer'))
 				case 'double':
 				case 'string':
 				case 'NULL':
-					return ctcSerializer::get_element_value($data,$settings);
+					return htmlspecialchars((string)$data,ENT_QUOTES);
 				case 'resource':
 					return 'resource';
-				case 'unknown type':
 				default:
 					return '';
 			}
 		}
 
-		private static function get_element_name($tag_name = '',$settings = array(),$is_root_element = false)
+		private static function get_element_name($element_name = '',$fallback_element_name = '')
 		{
-			$tag_name = ltrim((string)$tag_name,'1234567890');
-
-			if (strlen($tag_name) > 0)
-				return $tag_name;
-			else if ($is_root_element === true)
-				return $settings['rootName'];
+			$element_name = (string)$element_name;
+			if (class_exists('DOMElement'))
+			{
+				try
+				{
+					new DOMElement($element_name);
+					return $element_name;
+				}
+				catch (Exception $e)
+				{
+					return $fallback_element_name;
+				}
+			}
+			else if (preg_match('/\A(?!XML)[a-z][\w0-9-]*$/i',$element_name))
+				return $element_name;
 			else
-				return $settings['defaultTagName'];
-		}
-
-		private static function get_element_value($tag_value = '',$settings = array())
-		{
-			return htmlspecialchars((string)$tag_value,ENT_QUOTES);
+				return $fallback_element_name;
 		}
 	}
 }
